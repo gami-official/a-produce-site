76254185119dcfb2e5724d2226c3d7e99fed354a
gami-official <k_fuchigami@gamigami.email>
2026-04-14 21:32:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,9 @@ export default function Home() {
       {/* 物流 */}
       <section className="bg-white text-black py-20 px-6">
         <div className="max-w-6xl mx-auto">
-          <h2 className="text-3xl font-bold text-center">法人向け物流サービス</h2>
+          <h2 className="text-3xl font-bold text-center">
+            法人向け物流サービス
+          </h2>
 
           <div className="mt-10">
             <img
